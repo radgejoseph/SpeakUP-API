b866075a11f7940cb02f35b91919b0a4c4732a1b
radgejoseph <radgejoseph.sanjoaquin@gmail.com>
2021-05-22 00:02:29 +0800

    update queries/ test

--- test.php
+++ test.php
@@ -9,7 +9,7 @@ require_once 'connect.php';
 if (mysqli_connect_errno()){
     die('Unable to connect to datebase ' . mysqli_connect_error());
 }
-$sql = "SELECT puvs.vehicle, puvs.body_plate, IFNULL(ROUND(AVG(reviews.ratings)),0) FROM puvs LEFT JOIN reviews ON puvs.body_plate=reviews.body_plate  WHERE puvs.vehicle='jeep' GROUP BY puvs.body_plate";
+$sql = "SELECT vehicles.vehicle, vehicles.body_plate, IFNULL(ROUND(AVG(reviews.ratings)),0) FROM vehicles LEFT JOIN reviews ON vehicles.body_plate=reviews.body_plate  WHERE vehicles.vehicle='taxicle' GROUP BY vehicles.body_plate";
 
 $result = mysql_query($sql);
 echo [$result];
