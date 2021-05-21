410c169fa5ab3f179138a7d3cd89c703b6dbd43e
radgejoseph <radgejoseph.sanjoaquin@gmail.com>
2021-05-22 01:16:51 +0800

    change db connect

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 
 //$conn = mysqli_connect("localhost", "root", "", "speakup");
 $conn = mysqli_connect(
-    "remotemysql.com",
-    "fKjE0CChQb",
-    "fiDmb2Jzyd",
-    "fKjE0CChQb",
-    "3306");
+    "ec2-52-21-252-142.compute-1.amazonaws.com",
+    "fytmxcadqxmhxv",
+    "c379347585a8ec0dc7d94d5758bd4bb4e3f80df9971099c316903012d92fa95b",
+    "db41qemd6gkc0g",
+    "5432");
 ?>
