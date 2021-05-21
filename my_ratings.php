30322225fe9dda27a507c44f7733b541ce27ae7b
radgejoseph <radgejoseph.sanjoaquin@gmail.com>
2021-05-22 02:28:03 +0800

    change db connect

--- my_ratings.php
+++ my_ratings.php
@@ -9,18 +9,17 @@ $id = $_POST['id'];
 	}
 	
 	//$stmt = $conn->prepare("SELECT vehicle, body_plate, narrative, ratings FROM reviews WHERE user_id='1';");
-	$stmt = $conn->prepare("SELECT vehicle, body_plate, narrative, ratings FROM reviews WHERE user_id='$id';");
+	$stmt = $conn->prepare("SELECT body_plate, narrative, ratings FROM reviews WHERE user_id='$id';");
 	
 	$stmt->execute();
 	
-	$stmt->bind_result($vehicle, $body_plate, $narrative, $ratings);
+	$stmt->bind_result( $body_plate, $narrative, $ratings);
  
  $items = array(); 
  
  //traversing through all the result 
  while($stmt->fetch()){
  $temp = array();
- $temp['vehicle'] = $vehicle; 
  $temp['body_plate'] = $body_plate;
  $temp['narrative'] = $narrative; 
  $temp['ratings'] = $ratings; 
--- review.php
+++ review.php
@@ -8,11 +8,10 @@ if ($_SERVER['REQUEST_METHOD'] =='POST'){
     $body_plate = $_POST['body_plate'];
 	$ratings = $_POST['ratings'];
 	$narrative = $_POST['narrative'];
-	$vehicle = $_POST['vehicle'];
 
     require_once 'connect.php';
 
-    $sql = "INSERT INTO reviews (user_id, body_plate, ratings, narrative, vehicle) VALUES ('$user_id', '$body_plate', '$ratings', '$narrative', '$vehicle')";
+    $sql = "INSERT INTO reviews (user_id, body_plate, ratings, narrative) VALUES ('$user_id', '$body_plate', '$ratings', '$narrative')";
 
     if ( mysqli_query($conn, $sql) ) {
         $result["success"] = "1";
