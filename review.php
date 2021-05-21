46794f8d7673bd34b72b004742c83eb1ec134372
radgejoseph <radgejoseph.sanjoaquin@gmail.com>
2021-05-22 04:12:01 +0800

    move to localhost connect, and bug fixes

--- review.php
+++ review.php
@@ -8,10 +8,11 @@ if ($_SERVER['REQUEST_METHOD'] =='POST'){
     $body_plate = $_POST['body_plate'];
 	$ratings = $_POST['ratings'];
 	$narrative = $_POST['narrative'];
+	$vehicle = $_POST['vehicle'];
 
     require_once 'connect.php';
 
-    $sql = "INSERT INTO reviews (user_id, body_plate, ratings, narrative) VALUES ('$user_id', '$body_plate', '$ratings', '$narrative')";
+    $sql = "INSERT INTO reviews (user_id, body_plate, ratings, narrative, vehicle) VALUES ('$user_id', '$body_plate', '$ratings', '$narrative', '$vehicle')";
 
     if ( mysqli_query($conn, $sql) ) {
         $result["success"] = "1";
