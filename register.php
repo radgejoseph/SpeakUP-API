f69de64ab608fb5dcf3bb82ebdbe2ffb00492106
radgejoseph <radgejoseph.sanjoaquin@gmail.com>
2021-05-22 18:09:29 +0800

    connect.php to live DB

--- register.php
+++ register.php
@@ -1,8 +1,5 @@
 <?php
-require_once 'connect.php';
-if (mysqli_connect_errno()){
-    die('Unable to connect to datebase ' . mysqli_connect_error());
-}
+
 if ($_SERVER['REQUEST_METHOD'] =='POST'){
 
     $name = $_POST['name'];
@@ -14,20 +11,22 @@ if ($_SERVER['REQUEST_METHOD'] =='POST'){
 
     $password = password_hash($password, PASSWORD_DEFAULT);
 
+    require_once 'connect.php';
+
     $sql = "INSERT INTO mobileappusers (name, username, password, phone_number, email, address, status) 
 	VALUES ('$name', '$username', '$password', '$phone_number', '$email', '$address', 'Not Verified')";
 
     if ( mysqli_query($conn, $sql) ) {
-        $result["success"] = "0";
-        $result["message"] = "error";
+        $result["success"] = "1";
+        $result["message"] = "success";
 
         echo json_encode($result);
         mysqli_close($conn);
 
     } else {
 
-        $result["success"] = "1";
-        $result["message"] = "success";
+        $result["success"] = "0";
+        $result["message"] = "error";
 
         echo json_encode($result);
         mysqli_close($conn);
